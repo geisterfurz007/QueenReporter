582c658c92468a1466954ffbcca7fa64c433b1c9
geisterfurz007 <geisterfurz007@gmail.com>
2018-04-14 17:22:49 +0200

    autoupdate added

--- QueenReporter.user.js
+++ QueenReporter.user.js
@@ -7,6 +7,8 @@
 // @include	 https://stackoverflow.com/*
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
+// @downloadURL https://github.com/geisterfurz007/JavaRoomStockComments/raw/master/QueenReporter.user.js
+// @updateURL https://github.com/geisterfurz007/JavaRoomStockComments/raw/master/QueenReporter.meta.js
 // ==/UserScript==
 
 const room = 111347;
