63fe67f7f36e88174b3a56f27d3ce2c92ce18feb
geisterfurz007 <geisterfurz007@users.noreply.github.com>
2018-09-20 07:39:40 +0200

    Did. Derp. A. Little.

--- QueenReporter.meta.js
+++ QueenReporter.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Queen Reporter
 // @namespace    https://github.com/geisterfurz007
-// @version      0.8.3
+// @version      0.8.5
 // @description  Quick feedback to Heat Detector
 // @author       geisterfurz007
 // @include	https://stackoverflow.com/*
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
-// @downloadURL https://github.com/geisterfurz007/QueenReporter/raw/master/QueenReporter.user.js
-// @updateURL https://github.com/geisterfurz007/QueenReporter/raw/master/QueenReporter.meta.js
+// @downloadURL https://github.com/SOBotics/QueenReporter/raw/master/QueenReporter.user.js
+// @updateURL https://github.com/SOBotics/QueenReporter/raw/master/QueenReporter.meta.js
 // ==/UserScript==
